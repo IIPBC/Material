0bda742ac342c49075ddb2e3f690e7430428c8d0
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2017-02-14 12:25:04 -0200

    Solutions Hands-on 01
    Some codes don't work, but try them all and see what happens.

---
+++ HandsOn_01/20170213.txt.py
@@ -0,0 +1,119 @@
+# coding: utf-8
+h="Hello"
+w="world"
+h="Hello "
+w="world "
+d="2017"
+print (h + w +d)
+h + w + d
+print("hello "+"world "+"2017")
+print("hello"+' '+"world"+' '+"2017")
+my_list=range(1:11)
+my_list=range(1,11)
+my_list
+my_list.append(11)
+my_list
+my_list=my_list[::-1]
+my_list
+my_list2 = range(1,11)
+my_list2 = my_list2.inverse
+my_list2 = my_list2.inverse()
+my_list2 = my_list2.reverse()
+print my_list2
+my_list2 = range(1,11)
+my_list2.inverse
+my_list2.inverse()
+get_ipython().magic(u'clear ')
+my_list2 = range(1,11)
+my_list2.reverse()
+my_list2
+import numpy
+numpy.array(my_list2)
+my_list=numpy.array(my_list2)
+def square (x) :
+    var=numpy.square(x)
+    return var
+square(my_list)
+for x in square(my_list):
+    if x>50
+for x in square(my_list):
+    if x>50:
+        i=i+1
+        
+i=0
+for x in square(my_list):
+    if x>50:
+        i=i+1
+        
+i
+print sum(square(my_list)>50)
+sum([1 for x in square(my_list) if x > 50])
+sum([x for x in square(my_list) if x > 50])
+sum([True, False, True])
+sum([x > 50 for x in square(my_list) if x > 50])
+sum([x > 50 for x in square(my_list)])
+for x in square(my_list):
+    if x>50:
+        i=i+1
+        
+CLEAR
+get_ipython().magic(u'clear ')
+USP = {"EACH":"eSCOLA DE AaRTES...",
+"FFLCH":"faculdade de Filosofia...}
+USP = {"EACH":"eSCOLA DE AaRTES...",
+"FFLCH":"faculdade de Filosofia..."}
+USP["EACH"]
+USP.update({"IF":"INSTITUTO BLA BLA"})
+USP
+dict(keys=['a','b'], values=[0,1])
+dict_test = dict(keys=['a','b'], values=[0,1])
+dict['a']
+dic_test['a']
+dict_test['a']
+dict_test = dict(zip(['a','b'], [0,1]))
+dict_test
+zip(['a','b'], [0,1])
+n=100
+sampledict = {}
+for x in range(1,n):
+    sampledict.update({x:x**2})
+    
+print sampledict
+for x in range(1,n):
+    sampledict[n]=n*n
+    
+sampledict
+n
+dict2 = dict([(i, i**2) for i in range(1, n)])
+dict2
+dict2 = dict([(i, i**2) for i in range(1, n)])
+a = maine coon
+a =" maine coon"
+a = a.title
+print(a)
+a = a.title()
+a ="maine coon"
+a.title()
+a
+a.title()
+a.inverse()
+help(str)
+a[::-1]
+a = a.title
+"maine coon".a()
+str.title()
+a("main coon")
+get_ipython().magic(u'clear ')
+var = list('Bitcheti')
+var
+var[-1] = 'e'
+var
+"".join(var)
+" ".join(var)
+"-".join(var)
+get_ipython().magic(u'history > 20170213.txt')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'ls h*')
+get_ipython().magic(u'history > "20170213.txt"')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'ls h*')
