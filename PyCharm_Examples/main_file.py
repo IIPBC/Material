96f1ad94cb0c71319b7685abb1295be4ce3d83db
Bruno Quint <bquint@ctio.noao.edu>
2017-02-13 23:00:18 -0200

    Added notebooks and Matplotlib presentation

---
+++ PyCharm_Examples/main_file.py
@@ -0,0 +1,8 @@
+import numpy
+import my_methods
+
+x = numpy.arange(10)
+y = numpy.sin(x)
+
+# ToDo Fix PEP8
+my_methods.Say_Hello_World()
---
+++ PyCharm_Examples/my_methods.py
@@ -0,0 +1,8 @@
+def say_hello():
+    """A very sad method calling you back"""
+    # Just a new line
+    print "Hello... it's me..."
+
+
+def say_hello_world():
+    print "Hello World!"
